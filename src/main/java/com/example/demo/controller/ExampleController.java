2522d9ff1dfca735157c4a1c73719cc1ecf8c681
Jacques Andre <jacquesandre@mailbox.org>
2023-01-10 20:51:37 +0000

    [feature] added consume endpoint

--- src/main/java/com/example/demo/controller/ExampleController.java
+++ src/main/java/com/example/demo/controller/ExampleController.java
@@ -2,7 +2,10 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Vector;
 
 @RestController
 @RequestMapping("/api")
@@ -11,6 +14,18 @@ public class ExampleController {
     @GetMapping("/test")
     public static String test(){
         return "test";
+    }
+
+    @GetMapping("/consume")
+    public static long consume(@RequestParam(name = "i") Integer iterations) {
+        Vector v = new Vector();
+
+        for (int i = 0; i < iterations ; i++) {
+            byte b[] = new byte[1048576];
+            v.add(b);
+        }
+        Runtime rt = Runtime.getRuntime();
+        return rt.freeMemory();
     }
 
 }
